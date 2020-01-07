c32af354a4db673343b6613f97958839fd362b81
Kirsten McMillan <kirsten.marie.mcmillan@gmail.com>
2020-01-07 12:43:40 +0000

    add license

--- R/sum_squares.R
+++ R/sum_squares.R
@@ -1,3 +1,13 @@
+#' Calculate the sums if squares fir a sample
+#'
+#' #@param v numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' sum_squares(rnorm(50))
+
 sum_squares <- function(v) {
   sum((v - mean(v))^2)
 }
