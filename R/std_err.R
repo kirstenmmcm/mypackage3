847f98f5e35185f27586b462ba41f75cd6c29c12
Kirsten McMillan <kirsten.marie.mcmillan@gmail.com>
2020-01-07 13:28:09 +0000

    adding new function

---
+++ R/std_err.R
@@ -0,0 +1,17 @@
+#' Calculate the standard error for a sample
+#'
+#'returns the standard error of
+#'a numeric vector
+#'
+#' @param v numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' std_err(rnorm(50))
+#'
+#' @importFrom stats sd
+std_err <- function(v) {
+  sd(v)/sqrt(length(v))
+}
